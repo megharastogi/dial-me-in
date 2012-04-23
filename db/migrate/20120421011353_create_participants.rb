01cbed43715618d0888ae868d28f1103d4a2e4c4
Andy Zhang <signupforandy@gmail.com>
2012-04-22 21:45:04 -0700

    Fixed Participant table
    Change phone from number to string, migrated

--- db/migrate/20120421011353_create_participants.rb
+++ db/migrate/20120421011353_create_participants.rb
@@ -2,7 +2,7 @@ class CreateParticipants < ActiveRecord::Migration
   def change
     create_table :participants do |t|
       t.string :name
-      t.number :phone
+      t.string :phone
 
       t.timestamps
     end
