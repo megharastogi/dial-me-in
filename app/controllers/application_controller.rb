bce7edfe181b7c45c8a21d2074f5d6fca4ca78e5
Andy Zhang <signupforandy@gmail.com>
2012-04-28 15:33:13 -0700

    AppController uploads IronWorker code and CallsController creates Iron schedule after call is saved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
   	private_config = YAML.load_file('config/private_config.yml')
   	@twilio_client = Twilio::REST::Client.new private_config['twilio']['account_sid'], private_config['twilio']['auth_token']
   	@iron_client = IronWorkerNG::Client.new token: private_config['iron']['token'], project_id: private_config['iron']['project_id']
+  	# Upload/update IronWorker
+  	@iron_client.codes.create(IronWorkerNG::Code::Ruby.new 'app/workers/call_trigger.rb')
   end
 end
